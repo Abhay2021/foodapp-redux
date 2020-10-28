49591a970d0d9b78d479295ac510db288aa021e2
Randhir <rb.roy98@gmail.com>
2020-10-28 18:25:38 +0530

    Updates

---
+++ precache-manifest.5132bdc7fcadb4edce542b13a082fe92.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5a73aab7c43206c0ae41851bcf7808",
+    "url": "/foodapp-redux/index.html"
+  },
+  {
+    "revision": "de87693a8a6968f72887",
+    "url": "/foodapp-redux/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "71b0c9220733022a1fca",
+    "url": "/foodapp-redux/static/css/main.ea50e72a.chunk.css"
+  },
+  {
+    "revision": "de87693a8a6968f72887",
+    "url": "/foodapp-redux/static/js/2.720cb639.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/foodapp-redux/static/js/2.720cb639.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71b0c9220733022a1fca",
+    "url": "/foodapp-redux/static/js/main.808f7165.chunk.js"
+  },
+  {
+    "revision": "b2dfa26d9f17b9e52a4c",
+    "url": "/foodapp-redux/static/js/runtime-main.f65e48d0.js"
+  },
+  {
+    "revision": "6b8caf6f693c91114afa25d4b106ca7e",
+    "url": "/foodapp-redux/static/media/banner.6b8caf6f.jpg"
+  },
+  {
+    "revision": "8cb4c37ecdc70b3d0ee2b60b3b2f7fbe",
+    "url": "/foodapp-redux/static/media/burger.8cb4c37e.jpg"
+  },
+  {
+    "revision": "cb463d9c83ff4d659947e67098163ff0",
+    "url": "/foodapp-redux/static/media/chicken.cb463d9c.jpg"
+  },
+  {
+    "revision": "f5bd388b311cf5f4790a6c154af57943",
+    "url": "/foodapp-redux/static/media/chickenbirynai.f5bd388b.jpg"
+  },
+  {
+    "revision": "fa5c54d68afdf714c06bfbed65e208e2",
+    "url": "/foodapp-redux/static/media/coffee.fa5c54d6.jpg"
+  },
+  {
+    "revision": "cdc01a2807e18bab2ec53d098b6beed2",
+    "url": "/foodapp-redux/static/media/cold-coffee.cdc01a28.jpg"
+  },
+  {
+    "revision": "aa13e80d828018a06effed5728986509",
+    "url": "/foodapp-redux/static/media/dumplings.aa13e80d.jpg"
+  },
+  {
+    "revision": "a71f66c2f5c9ebbfc6451daecc49e205",
+    "url": "/foodapp-redux/static/media/friedfish.a71f66c2.jpg"
+  },
+  {
+    "revision": "8118aacd3a131b7fe8db7888fa1f632a",
+    "url": "/foodapp-redux/static/media/icecream.8118aacd.jpg"
+  },
+  {
+    "revision": "97b6c43bea36c7a8daa0eb767127b4e9",
+    "url": "/foodapp-redux/static/media/noodles.97b6c43b.jpg"
+  },
+  {
+    "revision": "9f8c39cc688a2f37a19f0af434cfeb8d",
+    "url": "/foodapp-redux/static/media/pasta.9f8c39cc.jpg"
+  },
+  {
+    "revision": "4b81dedadf5018543d23802e8e6822d5",
+    "url": "/foodapp-redux/static/media/piza.4b81deda.jpg"
+  },
+  {
+    "revision": "4c52a43c7f84c810e5be9b3d877a20b0",
+    "url": "/foodapp-redux/static/media/popcorn.4c52a43c.jpg"
+  },
+  {
+    "revision": "2728c62e18b77a19a19b94cfa3cffc98",
+    "url": "/foodapp-redux/static/media/sweet.2728c62e.jpg"
+  },
+  {
+    "revision": "38896a325377b77dbffa657e534d6125",
+    "url": "/foodapp-redux/static/media/tofu-noodles.38896a32.jpg"
+  },
+  {
+    "revision": "822cd4ce4784aede7b63b6b4683454e4",
+    "url": "/foodapp-redux/static/media/tofu-receipe.822cd4ce.jpg"
+  },
+  {
+    "revision": "6dc96543f430be20924a2dacf5c9007a",
+    "url": "/foodapp-redux/static/media/veg-roll.6dc96543.jpg"
+  }
+]);
